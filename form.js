bce1d91a582b06b6d08e15d845c6c1aa52774c77
SavithaThomas <Savitha.Thomas@fibonalabs.com>
2022-05-17 17:03:09 +0530

    form validation3

--- form.js
+++ form.js
@@ -1,19 +1,43 @@
 
+myform.addEventListener('submit', e => {
+    e.preventDefault();     
+    validation();    
+    }
+    );
 
+    function disablekeys(evt)
+    {
+
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode;
+
+    if (iKeyCode != 46 && iKeyCode > 32 && (iKeyCode < 65 || iKeyCode > 90) && (iKeyCode < 97 || iKeyCode > 122))
+  
+        return false;
+    else
+        return true;
+}
 
 function fnamecheck() {
-
+    
+    document.getElementById("fname1").innerHTML = "";
     var fnre = /^[a-zA-Z]+$/;
-    a=document.myform.fname.value;    
+    a=document.myform.fname.value.trim();    
     if ( !fnre.test(a) )
      {   
-     alert("First name must contain only characters");        
-     document.getElementById("fname").selected = true;    
-}
+     alert("First name must contain only characters");                
+    }
+    if  (a.length<5 )
+    {
+        document.getElementById("fname1").innerHTML = "First name has to be min 5 chars";
+
+            }
+    else
+        document.getElementById("fname1").innerHTML = "";
+
 }
 
 function fnlcheck() {
-    a=document.myform.fname.value;    
+    a=document.myform.fname.value.trim();    
     document.getElementById("fname").selected = true;   
     if (a == null || a == "") {
         window.alert("Please enter firstname")
@@ -38,14 +62,21 @@ function fnlcheck() {
     
 }
 function lnamecheck() {
-
+    document.getElementById("lname1").innerHTML = "";
     var fnre = /^[a-zA-Z]+$/;
-    b=document.myform.lname.value;    
+    b=document.myform.lname.value.trim();    
     if ( !fnre.test(b) )
      {   
-     alert("First name must contain only characters");        
-     document.getElementById("lname").selected = true;    
+     alert("Last name must contain only characters");        
+         
 }
+    if  (b.length<5 )
+    {
+        document.getElementById("lname1").innerHTML = "Last name has to be min 5 chars";
+
+            }
+    else
+        document.getElementById("lname1").innerHTML = "";
 }
 
 function lnlcheck() {
@@ -73,7 +104,7 @@ function lnlcheck() {
 function pnocheck() {
 
     var phre = /^[0-9]+$/;    
-    pho=document.myform.pno.value;    
+    pho=document.myform.pno.value.trim();    
     if ( !phre.test(pho) )
      {   
      alert("Phone no must contain only numbers");        
@@ -82,8 +113,9 @@ function pnocheck() {
 }
 
 function pnolcheck() {
-    pho=document.myform.pno.value;
-    document.getElementById("pno").selected = true;   
+    document.getElementById("pno1").innerHTML = "";
+    pho=document.myform.pno.value.trim();
+      
     // if (pho == null || pho == "") {
     //     window.alert("Please enter phonenumber")
     //     document.getElementById("pno").selected = true;   
@@ -91,13 +123,15 @@ function pnolcheck() {
     // } 
     if ( pho.length != 10 )
     {            
-        alert("Please enter 10 digit phone number ");         
-        document.getElementById("pno").selected = true;   
+        document.getElementById("pno1").innerHTML = "Phone number has to be 10 digits long";
+        //alert("Please enter 10 digit phone number ");         
+           
          }
     }
 
 function emailcheck() {
-    mail=document.myform.email.value;
+    mail=document.myform.email.value.trim();
+    document.getElementById("email1").innerHTML = "";
     var emre = /^\w+[\.]?\w+[@]+\w+[\.]+[a-zA-Z]+$/;
     if ( mail== null || mail == "") {
         window.alert("Please enter email")
@@ -105,8 +139,8 @@ function emailcheck() {
     } 
     
     else if ( !emre.test(mail) ) {
-        // document.getElementById("demo").innerHTML = "Email must contain @ characters";
-           alert("Email must contain @ & . characters");                       
+         document.getElementById("email1").innerHTML = "Email must contain @  & . characters";
+           //alert("Email must contain @ & . characters");                       
      }
  
 }
@@ -118,84 +152,107 @@ function passcheck()
     var pass1 = /[@#!$]+/;
     var pass2 = /[A-Z]+/;
     var pass3 = /[0-9]+/;
+    document.getElementById("pass1").innerHTML ="";
 
-    pass=document.myform.pwd.value;
+    pass=document.myform.pwd.value.trim();
 
     if (pass.length < 8){
-        window.alert("Password must be atleast 8 chars long")
-        document.getElementById("pass").selected = true;   
-        //document.getElementById("demo").innerHTML ="Password must be atleast 4 chars long";
+       
+        document.getElementById("pass1").innerHTML = "Password 8 chars long and must include small case,upper case and number";
+        //window.alert("Password must be atleast 8 chars long")
+        
     }    
      else if (pass == null || pass == "") {
-         window.alert("Please enter password")
-       // document.getElementById("demo").innerHTML ="Please enter password";
+         //window.alert("Please enter password")
+       document.getElementById("pass1").innerHTML ="Please enter password";
      } 
-     else if ( !passre.test(pass) ) {
-                                  
-        //document.getElementById("demo").innerHTML = "Password must contain an upper case letter , special charcter  and a number";
-         window.alert("Password must contain a small case letter ");                           
-    }     
+     else if ( !passre.test(pass) ) {                                  
+        document.getElementById("pass1").innerHTML = "Password must contain a small case letter";
+         //window.alert("Password must contain a small case letter ");                           
+    }    
     
     else if ( !pass1.test(pass))
     {
-    window.alert("Password must contain a special charcter");        
+        document.getElementById("pass1").innerHTML = "Password must contain a special character";
+        //window.alert("Password must contain a special charcter");        
     }
    else if ( !pass2.test(pass))
     {
-    window.alert("Password must contain an upper case letter"); 
+        document.getElementById("pass1").innerHTML = "Password must contain an upper case letter";
+    //window.alert("Password must contain an upper case letter"); 
     }       
 
     else if ( !pass3.test(pass))
    {
-
-    window.alert("Password must contain a number");     
+    document.getElementById("pass1").innerHTML ="Password must contain a number";
+    //window.alert("Password must contain a number");     
 }       
 
 }
 
+function myFunction() {
+    window.location.href="./appl.html";
+  }
+
 function msg1() {
     document.getElementById("demo").innerHTML = "Your values are getting cleared";
-    document.getElementById("pass").selected = true;   
+     
     // window.alert("") ;
  }
 
  function validation() {
-    a=document.myform.fname.value; 
-    b=document.myform.lname.value;  
-    pho=document.myform.pno.value;   
-    mail=document.myform.email.value;
-    pass=document.myform.pwd.value;
+    document.getElementById("submit").addEventListener("click", function(event){
+        event.preventDefault()
+      }); 
+    a=document.myform.fname.value.trim(); 
+    b=document.myform.lname.value.trim();  
+    pho=document.myform.pno.value.trim();   
+    mail=document.myform.email.value.trim();
+    pass=document.myform.pwd.value.trim();
+    var emre = /^\w+[\.]?\w+[@]+\w+[\.]+[a-zA-Z]+$/;
+    var passre = /[a-z]+/;
+    var pass1 = /[@#!$]+/;
+    var pass2 = /[A-Z]+/;
+    var pass3 = /[0-9]+/;
      
-    if (a == null || a == "") {
-        window.alert("Please enter firstname")
-        document.getElementById("fname").selected = true;   
+    if (a == null || a == "" ||  a.length<5) {
+        window.alert("Please enter firstname with min 5 chars")       
+    
       
     }     
-    else if (b == null || b == "") {
-        window.alert("Please enter lastname")
-        document.getElementById("lname").selected = true;   
+    else if (b == null || b == "" || b.length<5) {
+        window.alert("Please enter lastname with min 5 chars")
+        
       
     }        
-    else if (pho == null ||pho == "") {
-        window.alert("Please enter phonenumber")
-        document.getElementById("pno").selected = true; 
+    else if (pho == null ||pho == "" || pho.length<10) {
+        window.alert("Please enter 10 digit phonenumber")
+        
       
     }     
-    else if ( mail== null || mail == "") {
-        window.alert("Please enter email")
-        document.getElementById("email").selected = true;       
+    else if ( mail== null || mail == "" ||  !emre.test(mail) ) {
+        window.alert("Please enter email with special charc @ & .")
+         
     }     
-   else  if (pass == null || pass == "") {
+   else  if (pass == null || pass =="" ) {
         window.alert("Please enter password")
-        document.getElementById("pwd").selected = true; 
-      // document.getElementById("demo").innerHTML ="Please enter password";
+        document.getElementById("pwd").selected = true;     
+      
     } 
+    else if (!passre.test(pass) || !pass1.test(pass) || !pass2.test(pass) || !pass3.test(pass) ) {
+        window.alert("Password 8 chars long and must include small case,upper case and number")
+        document.getElementById("pwd").selected = true;     
+      
+    } 
+    else
+    {
+        
+        window.location.href="./appl.html";
+    }
+   
+   }
 
-    window.location="appl.html";
-    
-
- }
-
+  
 
 
 
